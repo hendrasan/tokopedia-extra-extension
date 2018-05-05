b0af18ead836df14cfc11e495c45ace02b3a36f7
Hendra Susanto <benhanks040888@gmail.com>
2018-05-05 18:18:40 +0700

    update

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,7 +4,7 @@ chrome.extension.sendMessage({}, function(response) {
 
 function init() {
   var observe = document.querySelector('._1AgQNQHv, ._1hoMwZCy, .category-products-all');
-  var items = document.querySelectorAll('._33JN2R1i, ._2zW9OOW2, .product-card-category');
+  var items = document.querySelectorAll('._33JN2R1i, ._2zW9OOW2, .product-card-category, .product-card');
 
   if (items) {
     Array.prototype.forEach.call(items, function(item, i){
@@ -17,7 +17,7 @@ function init() {
     var observer = new MutationObserver(function(mutations) {
       mutations.forEach(function(mutation) {
         Array.prototype.slice.call(mutation.addedNodes).forEach(function(el) {
-          if (el.classList.contains('_33JN2R1i') || el.classList.contains('_2zW9OOW2') || el.classList.contains('product-card-category')) {
+          if (el.classList.contains('_33JN2R1i') || el.classList.contains('_2zW9OOW2') || el.classList.contains('product-card-category') || el.classList.contains('product-card')) {
             getItemStat(el);
           }
         });
@@ -36,12 +36,24 @@ function init() {
 
 function getItemStat(item) {
   var pid;
+  var element;
 
-  if (item.classList.contains('_2zW9OOW2')) {
-    pid = item.querySelector('._2vNIys4S').value;
-  } else {
-    pid = item.getAttribute('data-pid');
+  pid = item.getAttribute('data-pid');
+
+  if (!pid) {
+    element = item.querySelector('div > span > button');
+    if (element) {
+      pid = element.value;
+    }
   }
+
+  if (!pid) {
+    element = item.querySelector('.icheck');
+    if (element) {
+      pid = element.value;
+    }
+  }
+
 
   var url = 'https://js.tokopedia.com/productstats/check?pid=' + pid;
 
@@ -50,9 +62,9 @@ function getItemStat(item) {
       var str = response.data;
       var jsonString = JSON.parse(str.substring(str.indexOf('(') + 1, str.indexOf(')')));
 
-      var el = item.querySelector('._21Anx7su > div:last-child, ._2AXi12hA, .product-summary');
+      var el = item.querySelector('.vlEGRFVq, ._2AXi12hA, .product-summary, .detail');
       el.style.height = "130px";
-      var html = '<span class="sold-count" style="color: #555555;font-size: 12px;"><i class="icon-shopping-cart-alt-2 mr-5"></i>Terjual: <strong>' + jsonString.item_sold + '</strong></span>';
+      var html = '<div class="sold-count" style="color: #555555; font-size: 12px; line-height: 28px;"><i class="icon-shopping-cart-alt-2 mr-5"></i>Terjual: <strong>' + jsonString.item_sold + '</strong></div>';
 
       if (el && !item.querySelector('.sold-count')) {
         el.insertAdjacentHTML('beforeend', html);
