dc6a1df0f000d26aa3982c80ef42d3e3c2ff266a
Hendra Susanto <benhanks040888@gmail.com>
2018-06-06 19:54:51 +0700

    update and bump version

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,7 +4,7 @@ chrome.extension.sendMessage({}, function(response) {
 
 function init() {
   var observe = document.querySelector('._1AgQNQHv, ._1hoMwZCy, .category-products-all');
-  var items = document.querySelectorAll('._33JN2R1i, ._2zW9OOW2, .product-card-category, .product-card');
+  var items = document.querySelectorAll('._33JN2R1i, ._2zW9OOW2, .product-card-category, .product-card, .shop-product, ._sdMeYlI');
 
   if (items) {
     Array.prototype.forEach.call(items, function(item, i){
@@ -17,7 +17,7 @@ function init() {
     var observer = new MutationObserver(function(mutations) {
       mutations.forEach(function(mutation) {
         Array.prototype.slice.call(mutation.addedNodes).forEach(function(el) {
-          if (el.classList.contains('_33JN2R1i') || el.classList.contains('_2zW9OOW2') || el.classList.contains('product-card-category') || el.classList.contains('product-card')) {
+          if (el.classList.contains('_33JN2R1i') || el.classList.contains('_2zW9OOW2') || el.classList.contains('product-card-category') || el.classList.contains('product-card') || el.classList.contains('_sdMeYlI')) {
             getItemStat(el);
           }
         });
@@ -62,7 +62,7 @@ function getItemStat(item) {
       var str = response.data;
       var jsonString = JSON.parse(str.substring(str.indexOf('(') + 1, str.indexOf(')')));
 
-      var el = item.querySelector('.vlEGRFVq, ._2AXi12hA, .product-summary, .detail');
+      var el = item.querySelector('.vlEGRFVq, ._2AXi12hA, .product-summary, .detail, ._2Dq1BF0i > div:last-child');
       el.style.height = "130px";
       var html = '<div class="sold-count" style="color: #555555; font-size: 12px; line-height: 28px;"><i class="icon-shopping-cart-alt-2 mr-5"></i>Terjual: <strong>' + jsonString.item_sold + '</strong></div>';
 
